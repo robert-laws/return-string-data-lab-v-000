ba5ddb4de6262d82bcb29189e6ce38f8a19414c2
Robert Laws <roblaws@me.com>
2019-06-05 16:05:14 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,4 +11,13 @@ class ProductsController < ApplicationController
     product = Product.find(params[:id])
     render plain: product.description
   end
+
+  def inventory
+    product = Product.find(params[:id])
+    if product.inventory > 0
+      render plain: "true"
+    else
+      render plain: "false"
+    end
+  end
 end
