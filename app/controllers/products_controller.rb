6d536c88a1dfb25e095aef66c58fbaa054b09d22
Robert Laws <roblaws@me.com>
2019-06-05 16:18:16 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,11 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
 
-    if @product
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
   end
 
   def description
